fa17f235b654fca4c4ccdd9a5fa58ba87f1a8d4a
Леонид <Fear-1995@mail.ru>
2023-09-13 16:56:23 +0300

    minor fixes

--- flask_app/app.py
+++ flask_app/app.py
@@ -5,6 +5,11 @@ candidates = utils.load_file()
 
 app = Flask(__name__)
 
+def handle_bad_request():
+    return 'такой страницы не существует', 400
+
+app.register_error_handler(404, handle_bad_request())
+
 @app.route("/")
 def page_index():
      output = "<pre>\n"
@@ -33,12 +38,15 @@ def page_candidate(id):
 @app.route("/skills/<skill>")
 def page_skills(skill):
     output = "<pre>\n"
+    flag = False
     for candidate in candidates:
-        if skill.lower() in candidate["skills"].lower():
+        if skill.lower() in candidate["skills"].lower().split(", "):
+            flag = True
             output += f"{candidate['name']}\n"
             output += f"{candidate['position']}\n"
-            output += f"{candidate['skills']}\n"
-            output += "<pre>\n"
+            output += f"{candidate['skills']}\n<pre>\n"
+    if flag == False:
+        output+= "Ничего не найдено =(<pre>\n"
     return output
 
 app.run()
